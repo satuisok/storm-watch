f05a36b42907a9cca297614da5439db09e9c6821
Satu Isokaanta <satu.isokaanta@outlook.com>
2025-08-20 13:02:45 +0300

    Add error handling

--- telegram_message_bot.py
+++ telegram_message_bot.py
@@ -2,25 +2,32 @@ import pandas as pd
 import requests
 import os
 from dotenv import load_dotenv
-from weather_data import forecast_list
+from weather_data import fetch_forecast
 
 load_dotenv()
-
-# This is a test list for the Telegram to prevent unnecessary api calls from the OW API
-# forecast_list = [{'time': '12:00:00', 'status': 800}, {'time': '15:00:00', 'status': 212}, {'time': '18:00:00', 'status': 500}]
 
 storm_codes = pd.read_csv("storm_codes.csv")
 code_dict = dict(zip(storm_codes["code"], storm_codes["description"]))
 
-for weather in forecast_list:
-        # Send a Telegram message if the weather id is in storm codes
-        if weather["status"] in code_dict:
-                message = f"Expect: {code_dict[weather['status']]} today at {weather['time']} in {weather['location']}!"
-                url = f"https://api.telegram.org/bot{os.getenv("TELEGRAM_API")}/sendMessage"
-                payload = {
-                        'chat_id': os.getenv("TELEGRAM_BOT_ID"),
-                        'text': message
-                }
+# This is a test list for the Telegram to prevent unnecessary api calls from the OW API
+# forecast_list = [{'time': '12:00:00', 'status': 200, 'location': 'Koivusaari'}, {'time': '15:00:00', 'status': 500, 'location': 'Koivusaari'}, {'time': '18:00:00', 'status': 212, 'location': 'Koivusaari'}, {'time': '12:00:00', 'status': 500, 'location': 'Näköalapaikka'}, {'time': '15:00:00', 'status': 500, 'location': 'Näköalapaikka'}, {'time': '18:00:00', 'status': 500, 'location': 'Näköalapaikka'}]
 
-                response = requests.get(url, params=payload)
-                print(response.status_code)
+
+forecast_list = fetch_forecast()
+
+if forecast_list:
+        for weather in forecast_list:
+                # Send a Telegram message if the weather id is in storm codes
+                if weather["status"] in code_dict:
+                        message = f"Expect: {code_dict[weather['status']]} today at {weather['time']} in {weather['location']}!"
+                        url = f"https://api.telegram.org/bot{os.getenv("TELEGRAM_API")}/sendMessage"
+                        payload = {
+                                'chat_id': os.getenv("TELEGRAM_BOT_ID"),
+                                'text': message
+                        }
+
+                        response = requests.get(url, params=payload)
+                        print(response.status_code)
+else:
+        print("Missing forecast.")
+
--- weather_data.py
+++ weather_data.py
@@ -5,30 +5,35 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-for coord in locations:
-
-        parameters = {
-                "lat": coord["lat"],
-                "lon": coord["lon"],
-                "units": "metric",
-                "cnt": 3,
-                "appid": os.getenv("API_KEY")
-            }
-
-
-        weather_endpoint = requests.get("https://api.openweathermap.org/data/2.5/forecast", params=parameters)
-        weather_endpoint.raise_for_status()
-        weather_data = weather_endpoint.json()["list"] # retrieves directly the most important list from the json data.
-        print(weather_data)
-
+def fetch_forecast():
         forecast_list = []
-        for data in weather_data:
-                time = data["dt_txt"].split(" ")[1]
-                weather_id = data["weather"][0]["id"]
-                forecast_list.append({"time":time, "status": weather_id, "location": coord["name"]})
+        for coord in locations:
 
+                parameters = {
+                        "lat": coord["lat"],
+                        "lon": coord["lon"],
+                        "units": "metric",
+                        "cnt": 3,
+                        "appid": os.getenv("API_KEY")
+                    }
 
+                try:
+                        weather_endpoint = requests.get("https://api.openweathermap.org/data/2.5/forecast", params=parameters)
+                        weather_endpoint.raise_for_status()
+                except requests.exceptions.Timeout:
+                        print(f"The request timed out. Try again later.")
+                else:
+                        weather_data = weather_endpoint.json()["list"] # retrieves directly the most important list from the json data.
+                        # print(weather_data)
 
+                        for data in weather_data:
+                                time = data["dt_txt"].split(" ")[1]
+                                weather_id = data["weather"][0]["id"]
+                                forecast_list.append({"time":time, "status": weather_id, "location": coord["name"]})
+
+        return forecast_list
+
+# fetch_forecast()
 
 
 
